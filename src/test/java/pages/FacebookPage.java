74c305747e6f94a71bbde9745ff2fc27d61b5f90
Mustafa Culha <merguven753@gmail.com>
2023-03-04 18:34:38 +0300

    Practice

--- src/test/java/pages/FacebookPage.java
+++ src/test/java/pages/FacebookPage.java
@@ -2,8 +2,14 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
 
 public class FacebookPage {
+    public FacebookPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
     @FindBy(xpath = "//input[@placeholder='E-posta veya Telefon Numarası']")
     public WebElement emailBox;
