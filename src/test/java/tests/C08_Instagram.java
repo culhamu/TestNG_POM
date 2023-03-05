063427313dd381468ec3cf9efebd1ce99df8ae5e
Mustafa Culha <merguven753@gmail.com>
2023-03-05 15:25:11 +0300

    Practice

--- src/test/java/tests/C08_Instagram.java
+++ src/test/java/tests/C08_Instagram.java
@@ -1,6 +1,8 @@
 package tests;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.github.javafaker.Faker;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.InstagramPage;
 import utilities.ConfigReader;
@@ -11,16 +13,26 @@ public class C08_Instagram extends TestBaseRapor {
 
     @Test
     public void instagramTest(){
+        extentTest=extentReports.createTest("Negatif Instagram testi ","Gecersiz bilgilerle giris yapılamamalı");
 
         InstagramPage instagramPage=new InstagramPage();
         Faker faker=new Faker();
 
         Driver.getDriver().get(ConfigReader.getProperty("instagramUrl"));
+        extentTest.info("Instagram anasayfasına gidildi");
 
         instagramPage.emailBox.sendKeys(faker.internet().emailAddress());
+        extentTest.info("email kutusuna geçersiz mail adresi girildi");
 
         instagramPage.passwordBox.sendKeys(faker.internet().password());
+        extentTest.info("password kutusunu geçersiz password girildi");
 
         instagramPage.loginButton.click();
+        extentTest.info("login butonuna basıldı");
+
+        Assert.assertTrue(instagramPage.loginButton.isDisplayed());
+        extentTest.pass("Gecersiz mail ve password ile giris yapılamadıgı test edildi ");
+
+
     }
 }
