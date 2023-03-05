4867eb1d1f3b3c45ba6c4ab9c74f045423e0c31f
Mustafa Culha <merguven753@gmail.com>
2023-03-05 15:50:42 +0300

    Practice

--- src/test/java/tests/C05_Ebay.java
+++ src/test/java/tests/C05_Ebay.java
@@ -13,19 +13,29 @@ public class C05_Ebay extends TestBaseRapor {
 
     @Test
     public void ebayTest(){
+        extentTest=extentReports.createTest("Ebay macbook bilgisayar özellikleri belirleme","istenen özelliklere sahip macbook alma testi");
         EbayPage ebayPage=new EbayPage();
 
         Driver.getDriver().get(ConfigReader.getProperty("ebayUrl"));
+        extentTest.info("ebay anasayfaya gidildi");
+
         ReusableMethods.moveToElement(ebayPage.electronicsMoveToElement);
+        extentTest.info("Mouse electronics sekmesinin üzerine getirildi");
 
         ebayPage.computers.click();
+        extentTest.info("Computers seçeneği seçildi");
 
         ReusableMethods.scrollToElement(ebayPage.macbook);
+        extentTest.info("Macbook'a kadar sayfa aşağı indirildi");
+
 
         ebayPage.macbook.click();
+        extentTest.info("Macbook seçildi");
 
         String ilkWHD=Driver.getDriver().getWindowHandle();
         ebayPage.macbookProduct.click();
+        extentTest.info("Uygun macbook ürünü seçildi");
+
         String ikinciWHD ="";
         Set<String> tumWHD=Driver.getDriver().getWindowHandles();
         for (String each:tumWHD) {
@@ -34,8 +44,9 @@ public class C05_Ebay extends TestBaseRapor {
             }
         }
         Driver.getDriver().switchTo().window(ikinciWHD);
-        Assert.assertTrue(ebayPage.resultText.isDisplayed());
+        extentTest.info("ikinci sayafaya geçildi");
 
-        Driver.quitDriver();
+        Assert.assertTrue(ebayPage.resultText.isDisplayed());
+        extentTest.pass("sonuç yazısının görünürlüğü test edildi");
     }
 }
